4359049ea5012ed345ae7aca328d6333763f2426
Zaza Monkey <soumare.aicha13@gmail.com>
2024-12-26 23:10:14 +0100

    burger menu

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const button = document.getElementById('menu-button');
-  const dropdown = document.getElementById('menu-dropdown');
+const checkbox = document.querySelector(".checkbox");
+const navMenu = document.querySelector(".navMenu");
 
-  button.addEventListener('click', () => {
-    dropdown.classList.toggle('hidden');
-  });
+checkbox.addEventListener("click", () =>{
+  checkbox.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll("navbar").forEach(n => n.addEventListener("click", () => {
+  checkbox.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
