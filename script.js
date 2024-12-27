c3ce8c28eccccd5670a7785a0e070c6b72cca1e1
Zaza Monkey <soumare.aicha13@gmail.com>
2024-12-27 18:47:14 +0100

    responsivité

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const checkbox = document.querySelector("#checkbox");
 const navMenu = document.querySelector(".navMenu");
+const navItem = document.querySelectorAll(".navItem");
+let links = document.querySelectorAll(".navMenu a")
 
-checkbox.addEventListener("click", () =>{
+checkbox.addEventListener("click", () => {
   checkbox.classList.toggle("active");
   navMenu.classList.toggle("active");
-})
+});
 
-document.querySelectorAll("navbar").forEach(n => n.addEventListener("click", () => {
-  checkbox.classList.remove("active");
-  navMenu.classList.remove("active");
-}))
+document.querySelectorAll("navbar").forEach((n) =>
+  n.addEventListener("click", () => {
+    checkbox.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+
