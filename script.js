8a67eac64431bfe702e2b006522c816f665762e9
Zaza Monkey <soumare.aicha13@gmail.com>
2025-01-12 18:27:59 +0100

    adding new infos

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let checkbox = document.querySelector('#checkbox');
 let navMenu = document.querySelector('.navMenu');
 let links = document.querySelectorAll('navItem');
+let body = document.body;
 
 checkbox.addEventListener("click", () => {
   checkbox.classList.toggle("active");
