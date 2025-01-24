cf85f885ad87d133b09dec137066a5bb073e01c4
agathism <soumare.aicha13@gmail.com>
2025-01-24 11:28:48 +0100

    adding infos

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let checkbox = document.querySelector('#checkbox');
 let navMenu = document.querySelector('.navMenu');
 let links = document.querySelectorAll('navItem');
-let body = document.body;
+
 
 checkbox.addEventListener("click", () => {
   checkbox.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+
 
 
 
