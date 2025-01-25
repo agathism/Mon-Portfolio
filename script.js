b93575456720e816bf29536bc165fd2dfb73c0e0
agathism <soumare.aicha13@gmail.com>
2025-01-25 01:15:36 +0100

    adding infos

--- script.js
+++ script.js
@@ -3,10 +3,30 @@ let navMenu = document.querySelector('.navMenu');
 let links = document.querySelectorAll('navItem');
 
 
-checkbox.addEventListener("click", () => {
-  checkbox.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
+// checkbox.addEventListener("click", () => {
+//   checkbox.classList.toggle("active");
+//   navMenu.classList.toggle("active");
+// });
+
+function toggleMenu() {
+  if (checkbox.classList.contains("showMenu")) {
+    checkbox.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+checkbox.addEventListener("click", toggleMenu);
+
+links.forEach( 
+  function(navItem) { 
+    navItem.addEventListener("click", toggleMenu);
+  }
+)
 
 
 
